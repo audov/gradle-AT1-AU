de6c359874db95ea067797210bd36348472d08ef
Neto test <audov@mail.ru>
2023-07-28 12:20:21 +0300

    testng completed

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,12 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    testImplementation("junit:junit:4.13")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
 }
 
 tasks.test {
-    useJUnitPlatform()
+
+    useJUnit()
 }
